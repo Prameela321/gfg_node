531269e9fa43134b44bb0b0ef303ef4b5712e10e
unknown <prameela.thandu@beatoapp.com>
2024-05-01 11:15:38 +0530

    str

--- javascrpt/strings.js
+++ javascrpt/strings.js
@@ -1,2 +1,11 @@
 let firstName = 'anshika';
-console.log(firstName.length)
+console.log(firstName.length);
+console.log(firstName.toUpperCase());
+console.log(firstName.toLowerCase());
+console.log(firstName.slice(0,-3));
+console.log(firstName.slice(-2,-4));
+
+function greet(name) {
+    console.log(`hello ${name}`);
+}
+  greet('John');
